b8037c31d0951b44a7403a16d1c3e7fe5898e471
henrikr <henrikr@7dd200ee-abf9-0310-b500-cd26ac2fe5a9>
2005-06-17 11:45:40 +0000

    Added LICENSE, README, INSTALL and GPL headers to the source files

--- trunk/autoiwconfig/Profiles.py
+++ trunk/autoiwconfig/Profiles.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: UTF8 -*-
 
+# auto-iwconfig 0.1 - an automatic wireless profile tool
+# Copyright (C) 2005 Henrik Ronellenfitsch
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
 import os, subprocess, ConfigParser, sys, tempfile
 
 CONFIG_FILE = '~/.auto-iwconfig.conf'
